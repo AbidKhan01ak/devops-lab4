141d1f8ae6a8fa52428af4e6535095bdb24b6499
Abid Khan <akkhan.abid.01@gmail.com>
2025-07-02 10:31:54 +0000

    Updated App.java

--- src/main/java/com/devopslab/abid/App.java
+++ src/main/java/com/devopslab/abid/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello from CI Triggered Build" );
+        System.out.println( "Name: Abid Rafique Khan" );
+        System.out.println( "BITS ID: 2021WA86438" );
+        System.out.println( "DevOps - Lab number 4" );
     }
 }
