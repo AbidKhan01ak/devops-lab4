76f741c3be3d58dd5a95f4eae815468c7b711ef9
AbidKhan01ak <akkhan.abid.01@gmail.com>
2025-07-02 16:12:21 +0530

    Updated App.java from local

--- src/main/java/com/devopslab/abid/App.java
+++ src/main/java/com/devopslab/abid/App.java
@@ -1,9 +1,5 @@
 package com.devopslab.abid;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
@@ -12,5 +8,6 @@ public class App
         System.out.println( "Name: Abid Rafique Khan" );
         System.out.println( "BITS ID: 2021WA86438" );
         System.out.println( "DevOps - Lab number 4" );
+	System.out.println( "Continuous Integration Build success");
     }
 }
